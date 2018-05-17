0411d135111c8d2405aa1ef8d7bc4444ba861d42
Farha B. Jawed <farhajw@gmail.com>
2018-05-17 02:16:15 -0700

    draft without much css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,18 @@ var lives;
 var wins = 0;
 var losses = 0;
 var count;
-var bands = ["MICHAEL JACKSON","SHAKIRA","MADONNA","MICHAEL JACKSON","SHANIA","RIHANNA","NIRVANA"];
+// var bands = ["MICHAEL JACKSON","SHAKIRA","MADONNA","MICHAEL JACKSON","SHANIA","RIHANNA","NIRVANA"];
+
+var band = [
+    {name: "MICHAEL JACKSON", image: "http://cdn.smehost.net/michaeljacksoncom-uslegacyprod/wp-content/uploads/2017/04/mj_stage.jpg"},
+    {name: "SHAKIRA", image: "https://suntimesmedia.files.wordpress.com/2017/12/esp-mus_latin_american_music_awards-nominaciones_71368687-e1514387448532.jpg?w=763"},
+    {name: "MADONNA", image: "https://upload.wikimedia.org/wikipedia/commons/3/38/Madonna_3_by_David_Shankbone-2.jpg"},
+    {name: "NIRVANA", image:"https://upload.wikimedia.org/wikipedia/commons/f/fe/Creed_%28band%29_in_2002.jpg"},
+    {name: "COLD PLAY", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Coldplay_-_Global-Citizen-Festival_Hamburg_14.jpg/1280px-Coldplay_-_Global-Citizen-Festival_Hamburg_14.jpg"}, 
+    {name: "SHANIA TWAIN", image: "https://upload.wikimedia.org/wikipedia/commons/0/08/ShaniaTwainJune2011_%28cropped1%29.jpg"},
+    {name: "RIHANNA", image: "https://c1.staticflickr.com/8/7714/27876724745_637288766f_b.jpg"}
+  ]; 
+
 
 var htmlWord="";
 var htmlWrongWord="";
@@ -19,7 +30,7 @@ var bgSound = new Audio("https://cs1.mp3.pm/listen/3239549/UHZZY2xhS0hXTjZqbUtmN
 document.onkeyup = function(event){
     console.log(event.key);
    
-    if(gameStart){       
+    if(gameStart && event.keyCode == 32){       
         correctGuesses = [];
         wrongGuesses = [];
         hasWon = false;
@@ -33,7 +44,9 @@ document.onkeyup = function(event){
       
         bgSound.play();
 
-        currentWord = bands[index];
+        // currentWord = bands[index];
+        currentWord = band[index].name;
+       
 
         for(var j = 0; j<currentWord.length;j++){
             if(currentWord[j]=== " "){
@@ -55,7 +68,7 @@ document.onkeyup = function(event){
     
     //game played
     //keyup was not for game start
-    else{
+    else if(!gameStart){
         //1. user presses a key
         //2. check whether valid key pressed
         userChoice = event.key.toUpperCase();
@@ -64,7 +77,7 @@ document.onkeyup = function(event){
         //loop until user dies or won
         if(lives > 0 || !hasWon){
             //valid input?
-            if(userChoice.charCodeAt(0) >= 65 && userChoice.charCodeAt(0) <= 90 && userChoice.length===1){
+            if(event.keyCode >= 65 && event.keyCode <= 90){
                     console.log("valid");
                     //1. Repeatition of letters?
                    if(!repeatLetter(userChoice)){
@@ -98,6 +111,9 @@ document.onkeyup = function(event){
                        }                           
                    }
             }           
+        }
+        else{
+            return;
         }
     }
     
@@ -151,9 +167,10 @@ document.onkeyup = function(event){
 
     function showScoreBoardDOM(){
         var correctAnswerHtml = "The correct answer is: "+currentWord;
+        var img = "<img src='"+band[index].image+"' class='band-img' />";
         if(hasWon){
             var winHtml = "CONGRATULATIONS! YOU HAVE SAVED HIM :) <br>"+ correctAnswerHtml+
-                          "<br>Press on any key to play again";
+                          "<br>"+img+"<br>Press spacebar to play again";
                             
             document.querySelector("#winMsg").innerHTML = winHtml;
             document.querySelector("#winMsg").style.display="block";
@@ -161,14 +178,14 @@ document.onkeyup = function(event){
         }
        else{
            var loseHtml = "OH NO! YOU HAVE HANGED HIM <br>"+ correctAnswerHtml+
-                        "<br>Press on any key to play again";
+                        "<br>"+img+"<br>Press spacebar to play again";
            document.querySelector("#loseMsg").innerHTML = loseHtml;
            document.querySelector("#loseMsg").style.display="block";
            document.querySelector("#loss").innerHTML = losses;
        }
        index++;
        gameStart=true;
-       if(index > bands.length-1){
+       if(index > band.length-1){
           index = 0;
        }     
     }
